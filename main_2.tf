8c3c8afccefb02cf4b8faa30e4a442478940c31c
DonAntonio22 <45952051+DonAntonio22@users.noreply.github.com>
2020-10-19 21:50:58 +0200

    Add files via upload

--- main_2.tf
+++ main_2.tf
@@ -1,10 +1,8 @@
 provider "exoscale" {
-  key    = var.key
-  secret = var.secret
+  key = var.exoscale_key
+  secret = var.exoscale_secret
 }
-
 # Variable secrion
-
 //auf welchem server, welchem land es sich befindet
 variable "zone" {
   default = "at-vie-1"
@@ -13,14 +11,14 @@ variable "zone" {
 variable "template" {
   default = "Linux Ubuntu 20.04 LTS 64-bit"
 }
-// description = Beschreibung
-variable "key" {
+variable "exoscale_key" {
   description = "Enter exoscalse key"
 }
-
-variable "secret" {
+variable "exoscale_secret" {
   description = "Enter exoscalse secret"
 }
+// description = Beschreibung
+
 // data definiert ein fertiges model, "exoscale_compute_template" ist die Type, Name der Ressource ist "ubuntu"
 # Data section
 data "exoscale_compute_template" "ubuntuVersion" {
@@ -49,8 +47,10 @@ resource "exoscale_instance_pool" "instancepoolCreation" {
 set -e
 
 export DEBIAN_FRONTEND=noninteractive
-
+# Source 1 Script (S1): https://gist.github.com/janoszen/7ced227c54d1c9e86a9c1cbd93a451f2
+# Source 2 Kommentare (S2): https://docs.docker.com/engine/install/ubuntu/
 # region Install Docker
+# (S2) Falls es neuere Pakete gibt herunterladen und installieren
 apt-get update
 apt-get install -y \
     apt-transport-https \
@@ -58,35 +58,28 @@ apt-get install -y \
     curl \
     gnupg-agent \
     software-properties-common
-
+# (S2) hier wird der docker-GPG-key generiert
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
+# (S2) line 64 shows the last 8 digits of GPG-key
 apt-key fingerprint 0EBFCD88
+# (S2) Erstellt ein repository für den docker
 add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+# (S2) installiert docker engine
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
 # endregion
 
 # region Launch containers
-
+# Source (S3): https://github.com/FH-Cloud-Computing/http-load-generator
+# (S3) generiert CPU Load und ermöglicht mir bei 8080 die Ansicht der Health das alles OK ist.
 # Run the load generator
 docker run -d \
   --restart=always \
   -p 8080:8080 \
   janoszen/http-load-generator:1.0.1
-
-# Run the node exporter
-docker run -d \
-  --restart=always \
-  --net="host" \
-  --pid="host" \
-  -v "/:/host:ro,rslave" \
-  quay.io/prometheus/node-exporter \
-  --path.rootfs=/host
-
 # endregion
 
 EOF
@@ -98,12 +91,14 @@ resource "exoscale_security_group" "sg" {
 }
 //security group ist container mit einer liste von regeln über "exoscale_security_group_rule" werden die regeln erstellt
 //ingress incoming pakete outgress outgoing pakete
+//start_port und end_port bleiben 8080 damit ich nicht eine ganze range für die security group verwende.
+//start_port und end_port müssen auf 8080 sein damit auch der Request von websiteLoadBalancer durch die Firewall durch kann
 resource "exoscale_security_group_rule" "http" {
   security_group_id = exoscale_security_group.sg.id
   type              = "INGRESS"
   protocol          = "tcp"
   cidr              = "0.0.0.0/0"
-  start_port        = 80
+  start_port        = 8080
   end_port          = 8080
 }
 
